5ad53ffe1be93b5da9bbe115e10fc90775719a12
Alexander Roessler <aroessler@tormach.com>
2015-11-03 13:17:50 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+import subprocess
+import sys
+
+# We must use setuptools, not distutils, because we need to use the
+# namespace_packages option for the "google" package.
+try:
+    from setuptools import setup, Extension, find_packages
+except ImportError:
+    try:
+        from ez_setup import use_setuptools
+        use_setuptools()
+        from setuptools import setup, Extension, find_packages
+    except ImportError:
+        sys.stderr.write(
+            "Could not import setuptools; make sure you have setuptools or "
+            "ez_setup installed.\n"
+        )
+        raise
+
+from distutils.command.clean import clean
+
+if sys.version_info[0] == 3:
+    # Python 3
+    from distutils.command.build_py import build_py_2to3 as build_py
+else:
+    # Python 2
+    from distutils.command.build_py import build_py as build_py
+from distutils.spawn import find_executable
+
+
+if __name__ == '__main__':
+    setup(name="pymachinetalk",
+          version="1.0",
+          description="Python bindings for Machinetalk",
+          url="https://github.com/strahlex/pymachinetalk",
+          namespace_packages=['pymachinetalk'],
+          packages=find_packages(),
+          install_requires=['setuptools'],
+          cmdclass={
+              'clean': clean,
+              'build_py': build_py,
+          }
+          )
