32c17b1f5d82a282ec03df1f80f51f5310fb2269
Alexander Rössler <mail@roessler.systems>
2017-04-21 18:44:03 +0200

    dns_sd: implemented new service discovery

--- pymachinetalk/dns_sd.py
+++ pymachinetalk/dns_sd.py
@@ -4,19 +4,38 @@ from zeroconf import ServiceBrowser, Zeroconf, ServiceInfo
 class Service(object):
     def __init__(self, type_=''):
         self.type = type_
-        self.domain = ''
-        self.base_type = ''
-        self.protocol = ''
+        self.domain = 'local'
+        self.base_type = 'machinekit'
+        self.protocol = 'tcp'
         self.name = ''
         self.uri = ''
         self.uuid = ''
         self.version = 0
-        self.ready = False
+        self._ready = False
 
         self.service_infos = []
 
+        # callback
+        self.on_ready_changed_cb = []
+
+    @property
+    def ready(self):
+        return self._ready
+
+    @ready.setter
+    def ready(self, value):
+        if value != self._ready:
+            self._ready = value
+            for cb in self.on_ready_changed_cb:
+                cb(value)
+
+    @property
+    def typestring(self):
+        return '_%s._%s.%s.' % (self.base_type, self.protocol, self.domain)
+
     def matches_service_info(self, info):
-        return self.type == info.properties['service']
+        return self.type == info.properties.get('service') \
+            and self.typestring in info.type
 
     def __eq__(self, other):
         if isinstance(other, ServiceInfo):
@@ -34,67 +53,151 @@ class Service(object):
                 break
         self._update()
 
+    def clear_service_infos(self):
+        self.service_infos = []
+        self._update()
+
     def _update(self):
         if len(self.service_infos) > 0:
-            self.ready = True
             info = self.service_infos[0]
-            self.name = info.name
-            self.uri = info.properties['uri']
+            self._set_all_values_from_service_info(info)
+            self.ready = True
         else:
             self.ready = False
+            self._init_all_values()
 
+    def _set_all_values_from_service_info(self, info):
+        self.name = info.name
+        self.uri = info.properties.get('dsn', '')
+        self.uuid = info.properties.get('uuid', '')
+        self.version = info.properties.get('version', '')
+
+    def _init_all_values(self):
+        self.name = ''
+        self.uri = ''
+        self.uuid = ''
+        self.version = 0
+
+class ServiceDiscoveryFilter(object):
+    def __init__(self, name='', txt_records={}):
+        self.name = name
+        self.txt_records = txt_records
+
+    def matches_service_info(self, info):
+        if not isinstance(info, ServiceInfo):
+            raise TypeError('must pass a ServiceInfo object')
+        match = True
+        if not self.name in info.name:
+            match = False
+        for name, value in self.txt_records.iteritems():
+            if not info.properties[name] == value:
+                match = False
+                break
+        return match
 
 class ServiceDiscovery(object):
-    def __init__(self, uuid='', service_type='machinekit'):
+    def __init__(self, service_type='machinekit', filter_=ServiceDiscoveryFilter()):
         self.service_type = service_type
+        self.filter = filter_
 
         self.is_ready = False
         self.services = []
         self.browser = None
+        self.zeroconf = None
 
     def _start_discovery(self):
-        zeroconf = Zeroconf()
+        self.zeroconf = Zeroconf()
         type_string = '_%s._tcp.local.' % self.service_type
-        self.browser = ServiceBrowser(zeroconf, type_string, self)
+        self.browser = ServiceBrowser(self.zeroconf, type_string, self)
 
     def _stop_discovery(self):
-        pass
-
-    def remove_service(self, zeroconf, type, name):
-        info = zeroconf.get_service_info(type, name)
+        if self.zeroconf:
+            self.zeroconf.close()
+            self.zeroconf = None
+        self.browser = None
         for service in self.services:
-            if service.matches_service_info(info):
+            service.clear_service_infos()
+
+    def remove_service(self, zeroconf, type_, name):
+        info = zeroconf.get_service_info(type_, name)
+        for service in self.services:
+            if self.filter.matches_service_info(info) and service.matches_service_info(info):
                 service.remove_service_info(info)
 
-    def add_service(self, zeroconf, type, name):
-        info = zeroconf.get_service_info(type, name)
+    def add_service(self, zeroconf, type_, name):
+        info = zeroconf.get_service_info(type_, name)
         for service in self.services:
-            if service.matches_service_info(info):
+            if self.filter.matches_service_info(info) and service.matches_service_info(info):
                 service.add_service_info(info)
 
-    def register(self, item):
+    def _verify_item_and_run(self, item, cmd):
         if isinstance(item, Discoverable):
             for service in item.services:
-                self.services.append(service)
+                cmd(service)
         elif isinstance(item, Service):
-            self.services.append(item)
+            cmd(item)
         else:
             raise TypeError('passed unregisterable item')
 
+    def register(self, item):
+        if self.is_ready:
+            raise RuntimeError('cannot register service when service discovery is already running')
+        self._verify_item_and_run(item, self.services.append)
+
+    def unregister(self, item):
+        if self.is_ready:
+            raise RuntimeError('cannot unregister service when service discovery is already running')
+        self._verify_item_and_run(item, self.services.remove)
+
     def start(self):
         if not self.browser:
+            self.is_ready = True
             self._start_discovery()
 
     def stop(self):
         if self.browser:
+            self.is_ready = False
             self._stop_discovery()
-
-    def ready(self):
-        if not self.is_ready:
-            self.is_ready = True
-            self.start()
 
 
 class Discoverable(object):
     def __init__(self):
-        self.services = []
+        self._services = []
+        self._services_ready = False
+
+        self.on_services_ready_changed_cb = []
+
+    @property
+    def services(self):
+        return self._services
+
+    def add_service(self, service):
+        if not isinstance(service, Service):
+            raise TypeError('only Service is supported')
+        self._services.append(service)
+        service.on_ready_changed_cb.append(self._update_services_ready)
+
+    def remove_service(self, service):
+        if not isinstance(service, Service):
+            raise TypeError('only Service is supported')
+        self._services.remove(service)
+        service.on_ready_changed_cb.remove(self._update_services_ready)
+
+    @property
+    def services_ready(self):
+        return self._services_ready
+
+    @services_ready.setter
+    def services_ready(self, value):
+        if value is not self._services_ready:
+            self._services_ready = value
+            for cb in self.on_services_ready_changed_cb:
+                cb(value)
+
+    def _update_services_ready(self, _):
+        ready = True
+        for service in self._services:
+            if not service.ready:
+                ready = False
+                break
+        self.services_ready = ready
--- pymachinetalk/tests/test_dns_sd.py
+++ pymachinetalk/tests/test_dns_sd.py
@@ -37,14 +37,85 @@ def test_registeringAnythingElseFails(sd):
 
     assert not item in sd.services
 
+def test_registeringWhenRunningThrowsError(dns_sd, sd):
+    service = dns_sd.Service()
+    def dummy():
+        pass
+    sd._start_discovery = dummy
+    sd.start()
+
+    try:
+        sd.register(service)
+    except RuntimeError:
+        assert True
+
+    assert service not in sd.services
+
+def test_unregisteringServiceDirectlyWorks(dns_sd, sd):
+    service = dns_sd.Service()
+    sd.register(service)
+
+    sd.unregister(service)
+
+    assert service not in sd.services
+
+def test_unregisteringServicesFromDiscoverableWorks(dns_sd, sd):
+    service = dns_sd.Service()
+    discoverable = dns_sd.Discoverable()
+    discoverable.services.append(service)
+    sd.register(discoverable)
+
+    sd.unregister(discoverable)
+
+    assert service not in sd.services
+
+def test_unregisteringAnythingElseFails(sd):
+    item = 34
+
+    try:
+        sd.unregister(item)
+    except TypeError:
+        assert True
+
+    assert not item in sd.services
+
+def test_unregisteringWhenRunningThrowsError(dns_sd, sd):
+    service = dns_sd.Service()
+    def dummy():
+        pass
+    sd._start_discovery = dummy
+    sd.start()
+
+    try:
+        sd.unregister(service)
+    except RuntimeError:
+        assert True
+
+    assert service not in sd.services
+
+class ServiceInfoFactory(object):
+    def create(self, base_type='machinekit', domain='local', sd_protocol='tcp',
+               name='Hugo on Franz', service='halrcomp', uuid='12345678',
+               host='127.0.0.1', protocol='tcp', port=12345,
+               version=0, properties=None):
+        from zeroconf import ServiceInfo
+        typestring = '_%s._%s.%s.' % (base_type, sd_protocol, domain)
+        dsn = '%s://%s:%s' % (protocol, host, port)
+        if properties is None:
+            properties = {'uuid': uuid,
+                          'service': service,
+                          'dsn': dsn,
+                          'version': version}
+        return ServiceInfo(type_=typestring,
+                           name='%s %s.%s' % (name, host, typestring) ,
+                           properties=properties,
+                           address=host,
+                           port=port)
+
 @pytest.fixture
 def zeroconf(mocker):
-    from zeroconf import ServiceInfo, Zeroconf
-    service_info = ServiceInfo(type_='_machinekit._tcp.local.',
-                               name='Foo on Bar 127.0.0.1._machinekit._tcp.local.',
-                               properties={'uuid': '12345678',
-                                           'service': 'halrcomp',
-                                           'uri': 'tcp://127.0.0.1:12345'})
+    from zeroconf import Zeroconf
+    service_info = ServiceInfoFactory().create()
     zeroconf_stub = mocker.stub(name='get_service_info')
     zeroconf_stub.return_value = service_info
     stub_object = Zeroconf()
@@ -58,7 +129,6 @@ def test_serviceDiscoveredUpdatesRegisteredServices(dns_sd, sd, zeroconf):
     sd.add_service(zeroconf, '_machinekit._tcp.local.', 'Foo on Bar 127.0.0.1._machinekit._tcp.local.')
 
     assert service.ready is True
-    assert service.uri == 'tcp://127.0.0.1:12345'
 
 def test_serviceDisappearedUpdatesRegistedServices(dns_sd, sd, zeroconf):
     service = dns_sd.Service(type_='halrcomp')
@@ -67,4 +137,221 @@ def test_serviceDisappearedUpdatesRegistedServices(dns_sd, sd, zeroconf):
     sd.add_service(zeroconf, '_machinekit._tcp.local.', 'Foo on Bar 127.0.0.1._machinekit._tcp.local.')
     sd.remove_service(zeroconf, '_machinekit._tcp.local.', 'Foo on Bar 127.0.0.1._machinekit._tcp.local.')
 
+    assert service.ready is False
+
+def test_stoppingServiceDiscoveryResetsAllServices(dns_sd, sd, zeroconf):
+    service1 = dns_sd.Service(type_='halrcomp')
+    sd.register(service1)
+    service2 = dns_sd.Service(type_='halrcmd')
+    sd.register(service2)
+    sd.browser = object()  # dummy
+    sd.add_service(zeroconf, '_machinekit._tcp.local.', 'Foo on Bar 127.0.0.1._machinekit._tcp.local.')
+
+    sd.stop()
+
+    assert service1.ready is False
+    assert service2.ready is False
+
+def test_serviceInfoSetsAllRelevantValuesOfService(dns_sd):
+    service = dns_sd.Service(type_='halrcomp')
+    service_info = ServiceInfoFactory().create(
+        name='Foo on Bar',
+        uuid='987654321',
+        version=5,
+        host='10.0.0.10',
+        protocol='tcp',
+        port=12456
+    )
+
+    service.add_service_info(service_info)
+
+    assert service.uri == 'tcp://10.0.0.10:12456'
+    assert service.name == service_info.name
+    assert service.uuid == '987654321'
+    assert service.version == 5
+
+def test_serviceInfoWithIncompleteValuesIsIgnoredByService(dns_sd):
+    service = dns_sd.Service(type_='launcher')
+    service_info = ServiceInfoFactory().create(properties={})
+
+    service.add_service_info(service_info)
+
+    assert service.uri == ''
+    assert service.uuid == ''
+    assert service.version == ''
+
+def test_removingServiceInfoResetsAllRelevantValuesOfService(dns_sd):
+    service = dns_sd.Service(type_='blahus')
+    service_info = ServiceInfoFactory().create()
+    service.add_service_info(service_info)
+
+    service.remove_service_info(service_info)
+
+    assert service.uri == ''
+    assert service.name == ''
+    assert service.uuid == ''
+    assert service.version == 0
+
+def test_clearingServiceInfosResetsValuesOfService(dns_sd):
+    service = dns_sd.Service(type_='foobar')
+    service.add_service_info(ServiceInfoFactory().create())
+    service.add_service_info(ServiceInfoFactory().create())
+
+    service.clear_service_infos()
+
+    assert service.ready == False
+    assert service.uri == ''
+
+def test_settingReadyPropertyOfServiceTriggersCallback(dns_sd):
+    cb_called = [False]
+    def cb(_):
+        cb_called[0] = True
+    service = dns_sd.Service(type_='halrcomp')
+    service.on_ready_changed_cb.append(cb)
+    service_info = ServiceInfoFactory().create()
+
+    service.add_service_info(service_info)
+
+    assert cb_called[0] is True
+
+def test_discoverableAddingServiceWorks(dns_sd):
+    discoverable = dns_sd.Discoverable()
+    service = dns_sd.Service(type_='foo')
+
+    discoverable.add_service(service)
+
+    assert service in discoverable.services
+
+def test_discoverableAddingAnythingElseFails(dns_sd):
+    discoverable = dns_sd.Discoverable()
+    item = object()
+
+    try:
+        discoverable.add_service(item)
+        assert False
+    except TypeError:
+        assert True
+
+    assert item not in discoverable.services
+
+def test_discoverableRemovingServiceWorks(dns_sd):
+    discoverable = dns_sd.Discoverable()
+    service = dns_sd.Service(type_='foo')
+
+    discoverable.add_service(service)
+    discoverable.remove_service(service)
+
+    assert service not in discoverable.services
+
+def test_discoverableRemvoingAnythingElseFails(dns_sd):
+    discoverable = dns_sd.Discoverable()
+    item = object()
+
+    try:
+        discoverable.remove_service(item)
+        assert False
+    except TypeError:
+        assert True
+
+    assert item not in discoverable.services
+
+def test_discoverableAllServicesReadySetServicesReady(dns_sd):
+    discoverable = dns_sd.Discoverable()
+    service1 = dns_sd.Service(type_='foo')
+    discoverable.add_service(service1)
+    service2 = dns_sd.Service(type_='bar')
+    discoverable.add_service(service2)
+
+    service1.ready = True
+    service2.ready = True
+
+    assert discoverable.services_ready is True
+
+def test_discoverableNotAllServicesReadyUnsetsServicesReady(dns_sd):
+    discoverable = dns_sd.Discoverable()
+    service1 = dns_sd.Service(type_='foo')
+    discoverable.add_service(service1)
+    service2 = dns_sd.Service(type_='bar')
+    discoverable.add_service(service2)
+
+    service1.ready = True
+    service2.ready = True
+    service1.ready = False
+
+    assert discoverable.services_ready is False
+
+def test_discoverableServicesReadyChangedCallsCallback(dns_sd):
+    cb_called = [False]
+    def cb(_):
+        cb_called[0] = True
+    discoverable = dns_sd.Discoverable()
+    discoverable.on_services_ready_changed_cb.append(cb)
+
+    discoverable.services_ready = True
+
+    assert cb_called[0] is True
+
+def test_serviceDiscoveryFilterAcceptCorrectUuid(dns_sd):
+    service_info = ServiceInfoFactory().create(uuid='987654321')
+    filter = dns_sd.ServiceDiscoveryFilter(txt_records={'uuid': '987654321'})
+
+    assert filter.matches_service_info(service_info) is True
+
+def test_serviceDiscoveryFilterRejectWrongUuid(dns_sd):
+    service_info = ServiceInfoFactory().create(uuid='123456789')
+    filter = dns_sd.ServiceDiscoveryFilter(txt_records={'uuid': '987654321'})
+
+    assert filter.matches_service_info(service_info) is False
+
+def test_serviceDiscoveryFilterAcceptFuzzyName(dns_sd):
+    service_info = ServiceInfoFactory().create(name='Hello World')
+    filter = dns_sd.ServiceDiscoveryFilter(name='Hello')
+
+    assert filter.matches_service_info(service_info) is True
+
+def test_serviceDiscoveryFilterAcceptExactMatchingName(dns_sd):
+    service_info = ServiceInfoFactory().create(name='Foo')
+    filter = dns_sd.ServiceDiscoveryFilter(name='Foo')
+
+    assert filter.matches_service_info(service_info) is True
+
+def test_serviceDiscoveryFilterRejectNonMatchingName(dns_sd):
+    service_info = ServiceInfoFactory().create(name='Carolus Rex')
+    filter = dns_sd.ServiceDiscoveryFilter(name='Adolfus Maximus')
+
+    assert filter.matches_service_info(service_info) is False
+
+def test_serviceDiscoveryFilterPassingWrongObjectFails(dns_sd):
+    filter = dns_sd.ServiceDiscoveryFilter()
+
+    try:
+        filter.matches_service_info(object())
+        assert False
+    except TypeError:
+        assert True
+
+def test_serviceDiscoveryFiltersOutDiscoveredServiceWithWrongUuid(dns_sd, sd, zeroconf):
+    service = dns_sd.Service(type_='halrcomp')
+    sd.register(service)
+    sd.filter = dns_sd.ServiceDiscoveryFilter(txt_records={'uuid': '87654321'})
+
+    sd.add_service(zeroconf, '_machinekit._tcp.local.', 'Machinekit on MyBox 12.0.0.1._machinekit._tcp.local.')
+
+    assert service.ready is False
+
+def test_serviceDiscoveryFiltersInDiscoveredServiceWithCorrectUuid(dns_sd, sd, zeroconf):
+    service = dns_sd.Service(type_='halrcomp')
+    sd.register(service)
+    sd.filter = dns_sd.ServiceDiscoveryFilter(txt_records={'uuid': '12345678'})
+
+    sd.add_service(zeroconf, '_machinekit._tcp.local.', 'SuperPrint 192.168.7.2._machinekit._tcp.local.')
+    assert service.ready is True
+
+def test_serviceDiscoveryFiltersInDisappearedServiceWithCorrectUuid(dns_sd, sd, zeroconf):
+    service = dns_sd.Service(type_='halrcomp')
+    sd.register(service)
+    sd.filter = dns_sd.ServiceDiscoveryFilter(txt_records={'uuid': '12345678'})
+
+    sd.add_service(zeroconf, '_machinekit._tcp.local.', 'SuperPrint 192.168.7.2._machinekit._tcp.local.')
+    sd.remove_service(zeroconf, '_machinekit._tcp.local.', 'SuperPrint 192.168.7.2._machinekit._tcp.local.')
     assert service.ready is False
