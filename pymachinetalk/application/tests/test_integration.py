2c5ff09156c10a87295c66c331f59b95eec38b4c
Alexander Rössler <mail@roessler.systems>
2018-05-29 19:01:18 +0200

    moved application integrations tests to application module

--- pymachinetalk/application/tests/test_integration.py
+++ pymachinetalk/application/tests/test_integration.py
@@ -1,23 +1,13 @@
 # coding=utf-8
 import pytest
 import sys
-
-
-@pytest.fixture
-def application():
-    from pymachinetalk import application
-    return application
-
-
-@pytest.fixture
-def dns_sd():
-    from pymachinetalk import dns_sd
-    return dns_sd
+from pymachinetalk import application
+from pymachinetalk import dns_sd
 
 
 @pytest.mark.skipif(sys.version_info >= (3, 0),
                     reason="Integration tests hang for some reason with Python3")
-def test_application_integration(application, dns_sd):
+def test_application_integration():
     status = application.ApplicationStatus()
     command = application.ApplicationCommand()
     error = application.ApplicationError()
