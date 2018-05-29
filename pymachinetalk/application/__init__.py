f398b6649b6d7352398111a3ecc752512fd5c17d
Alexander Rössler <mail@roessler.systems>
2018-05-29 16:43:09 +0200

    move application into module

---
+++ pymachinetalk/application/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+from .constants import *
+from .command import ApplicationCommand
+from .error import ApplicationError
+from .file import ApplicationFile
+from .status import ApplicationStatus
