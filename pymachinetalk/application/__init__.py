66aa8f488487f470ede5e66bff58e9f9b2331b08
Alexander Rössler <mail@roessler.systems>
2018-05-29 19:01:40 +0200

    implement ApplicationLog

--- pymachinetalk/application/__init__.py
+++ pymachinetalk/application/__init__.py
@@ -3,4 +3,5 @@ from .constants import *
 from .command import ApplicationCommand
 from .error import ApplicationError
 from .file import ApplicationFile
+from .log import ApplicationLog
 from .status import ApplicationStatus
--- pymachinetalk/application/tests/test_integration.py
+++ pymachinetalk/application/tests/test_integration.py
@@ -11,10 +11,12 @@ def test_application_integration():
     status = application.ApplicationStatus()
     command = application.ApplicationCommand()
     error = application.ApplicationError()
+    log = application.ApplicationLog()
     appfile = application.ApplicationFile()
 
     sd = dns_sd.ServiceDiscovery()
     sd.register(status)
     sd.register(command)
     sd.register(error)
+    sd.register(log)
     sd.register(appfile)
